ef723e7f35b219d0dbbf564a5a48a9c821245eb4
Thisara priyashan <127646961+Thisara007@users.noreply.github.com>
2024-12-25 06:50:33 +0000

    header and footer added

---
+++ footer.php
@@ -0,0 +1,14 @@
+
+<footer >
+		
+			@All rights reserved
+	
+		<div class="icons">
+		<a href="#"><i class="bi bi-facebook"></i></a>
+		<a href="#"><i class="bi bi-whatsapp"></i></a>
+		<a href="#"><i class="bi bi-telephone"></i></a>
+		</div>
+</footer>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,39 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+	<link href="Styles.css" rel="stylesheet">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css" rel="stylesheet">
+	<title>Document</title>
+</head>
+<body>
+		<header>
+			<input type="checkbox" name="" id="toggler">
+			<label for="toggler" class="menubar"><span class="material-symbols-outlined">menu</span></label>
+			<a href="#"> <img src="logo.webp" placeholder="logo"></a>
+			<nav class="navbar" style="width:500px;">
+				<li type="none" class="nav-item">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+				<li type="none" class="nav-item">
+                    <a class="nav-link" href="#">Products</a>
+                </li>
+				<li type="none" class="nav-item">
+                    <a class="nav-link" href="test2.html">AboutUs</a>
+                </li>
+				<li type="none" class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
+			</nav>
+			<div class="icons">
+				<a href="cart.php"><i class="bi bi-cart"></i></a>
+				<a href="#"><i class="bi bi-person"></i></a>
+			</div>
+
+
+	</header>
+
+    
