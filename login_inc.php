3c7fb5a0262bffe7f45aa93e0d14088a9642a79c
Thisara priyashan <127646961+Thisara007@users.noreply.github.com>
2025-01-07 11:22:57 +0000

    logic errors solveed

--- login_inc.php
+++ login_inc.php
@@ -1,10 +1,11 @@
 <?php 
-require_once "functions.php";
+echo "hello1";
+
 require_once "dbconfi.php";
-echo "hello";
+
 function loginUser($connect,$email,$password){
     $emailexist = emailExsist($connect,$email);
-    echo "hello";
+    echo "hello3";
     if($emailexist === false){
         echo "<script>alert('Error login !');</script>";
         echo "<script>window.location.href='login.php';</script>";
@@ -28,11 +29,29 @@ function loginUser($connect,$email,$password){
     }
     
 }
+function invalidEmil($email){
+    if(!filter_var($email,FILTER_VALIDATE_EMAIL)){
+        $result = true;
+    }else{
+        $result = false;
+    }
+    return $result;}
+    function emailExsist($connect,$email){
+        $sql = "SELECT * FROM users WHERE email='$email';"; 
+            $result =  mysqli_query($connect,$sql);
+            if($row = mysqli_fetch_assoc($result)){
+                $result = $row;
+                return $result;
+            }else{
+               $result = false;
+               return $result;
+            }
+    }
 
  if(isset($_POST["submit_login"])){
           $password = $_POST["password"];
           $email = $_POST["email"];
-          echo "hello";
+          echo "hello2";
           loginUser($connect,$email,$password); 
          }
 
